4592d8370fed43cb664c07701d65616d4e52c28f
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2024-12-04 01:25:25 +0800

    init routes

--- cmd/infinivest/main.go
+++ cmd/infinivest/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"github.com/KZY20112001/infinivest-backend/internal/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-
+	routes.RegisterRoutes(r)
 	r.Run()
 }
