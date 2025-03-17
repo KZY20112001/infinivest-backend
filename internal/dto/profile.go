e25a1c0c860f4233cd343b852194841c3d0155dd
KZY20112001 <43703608+KZY20112001@users.noreply.github.com>
2025-03-18 00:25:05 +0800

    feat; profile

--- internal/dto/profile.go
+++ internal/dto/profile.go
@@ -3,7 +3,12 @@ package dto
 type ProfileRequest struct {
 	FirstName  string `json:"first_name"`
 	LastName   string `json:"last_name,omitempty"`
-	Address    string `json:"address"`
 	ProfileUrl string `json:"profile_url,omitempty"`
 	ProfileID  string `json:"profile_id,omitempty"`
+
+	RiskTolerance     string  `json:"risk_tolerance,omitempty"`
+	InvestmentStyle   string  `json:"investment_style,omitempty"`
+	InvestmentHorizon string  `json:"investment_horizon,omitempty"`
+	AnnualIncome      float64 `json:"annual_income,omitempty"`
+	ExperienceLevel   string  `json:"experience_level,omitempty"`
 }
